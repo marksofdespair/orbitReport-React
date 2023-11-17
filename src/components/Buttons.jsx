866cb2a3e2f3644114cd642d82bf302132c2ee94
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-11-17 13:21:28 -0600

    base project complete

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,23 @@
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  const handleSatClick = (sat) => {
+    filterByType(sat);
+  };
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+      {displaySats.map((sat) => (
+        <button
+          onClick={() => handleSatClick(sat)}
+          key={sat}
+        >
+          {sat} Orbit
+        </button>
+      ))}
+      <button onClick={() => setSat(satData)}>
+        All Orbits
+      </button>
     </div>
   );
 };
