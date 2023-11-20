0b4bfe5f3003f547de0b47ab81eaac5af270d1b7
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-11-20 17:15:39 -0600

    Completed styling step (finally)

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,3 +1,4 @@
+import './styling.css'
 import satData from "./satData";
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,11 @@
+import './styling.css'
 import React from "react";
 
+// Arrow func with sat as a property
 const Table = ({ sat }) => {
   return (
     <table>
+      {/* Table header */}
       <thead>
         <tr>
           <th>Name</th>
@@ -11,12 +14,17 @@ const Table = ({ sat }) => {
           <th>Status</th>
         </tr>
       </thead>
+      {/* Table Body*/}
       <tbody>
+        {/* Map over the sat prop, the array of satellite data in satData.jsx*/}
         {sat.map((data) => (
+          // For each satellite data, create a table row with a unique key based on 'id'
           <tr key={data.id}>
             <td>{data.name}</td>
             <td>{data.type}</td>
             <td>{data.launchDate}</td>
+            {/* Display satellite status in a table cell, with text color being based on 'operational' property */}
+            {/* If 'operational' is true, display 'Active'; otherwise, display 'Inactive', using ternary op  ----v */}
             <td style={{ color: data.operational ? 'green' : 'red' }}>{data.operational ? 'Active' : 'Inactive'}</td>
           </tr>
         ))}
